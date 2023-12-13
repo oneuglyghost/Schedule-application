3cb1ac4208ccd2357b067c9d9fb22d016276f5bc
Bernardo Alonso <bernardo4430@gmail.com>
2023-12-13 12:39:13 -0600

    finished logic for storing and pulling data from local storage also added a function to display when new event is added

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 
   //change time-block color based on the time
-  function updateStyles(){
+    function updateStyles(){
     const currentHour =dayjs().hour();
     
     $(".time-block").each(function(){
@@ -27,7 +27,7 @@ $(document).ready(function(){
 
   //save button saves to local storage 
   $(".saveBtn").on("click", function(){
-    var description = $(this).siblings(".description").val();
+    const description = $(this).siblings(".description").val();
     //look for the parent element of the button 
     const timeBlockId= $(this).closest(".time-block").attr("id");
 
@@ -39,13 +39,39 @@ $(document).ready(function(){
       };
       // make data to string to save to local storage
       localStorage.setItem(timeBlockId, JSON.stringify(dataTosave));
-
     }
-
     // print id to console just to confirm
     console.log("time block ID:", timeBlockId);
+
+
+    // shows message if succesfully saved an item 
+    var hourText = $("#"+timeBlockId).text();
+    console.log("saved", hourText)
+
+    $("#saved").text("Added event to " + hourText )
+    // clears text after a couple of seconds 
+    setTimeout(function(){
+      $("#saved").text("");
+    },3000);
   })
 
+  function loadDataFromLocalStorage(){
+    $(".time-block").each(function(){
+      const timeBlockId= $(this).closest(".time-block").attr("id");
+      const storedData = localStorage.getItem(timeBlockId);
+
+      if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        const description = parsedData.description;
+
+        // set the items in the text area
+        $(this).find(".description").val(description);
+      }
+    })
+  }
+
+// call the function to load data to page
+loadDataFromLocalStorage();
 
 
   // shows the current date at the top of page
