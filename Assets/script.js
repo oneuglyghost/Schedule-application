c0933672aca5583129b9a4c253cd88c14ebace4a
Bernardo Alonso <bernardo4430@gmail.com>
2023-12-13 09:46:15 -0600

    added css to center header and added date for header

--- Assets/script.js
+++ Assets/script.js
@@ -21,3 +21,7 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+$(document).ready(function(){
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"))
+})
